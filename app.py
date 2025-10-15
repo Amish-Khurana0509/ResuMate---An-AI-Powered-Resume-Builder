d11207a6d57368cd44cb751c7ff7431d06ac33ed
Amish Khurana <amishkhurana09@gmail.com>
2025-10-16 00:24:34 +0530

    Update secret key and improve error handling

--- app.py
+++ app.py
@@ -2,7 +2,7 @@ from flask import Flask, render_template, request, jsonify
 import os
 
 app = Flask(__name__)
-app.secret_key = 'ResuMate2024'
+app.secret_key = 'ResuMate@MCS'
 
 COHERE_API_KEY = os.environ.get('COHERE_API_KEY', 'Ty8rnnDkH4qi3YRJeX2RL15qZxVURr64psQi5i8H')
 co = None
@@ -204,3 +204,4 @@ def get_basic_feedback(answers):
 if __name__ == '__main__':
     print("Starting ResuMate...")
     app.run(debug=True, port=5000, host='0.0.0.0')
+
